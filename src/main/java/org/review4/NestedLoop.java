6c5e843de40d258c33ad324ab29fd369a51c1d82
SyntaxTechnologies <instructor@syntaxtechs.com>
2023-11-20 21:53:14 -0500

    nested loop more examples

--- src/main/java/org/review4/NestedLoop.java
+++ src/main/java/org/review4/NestedLoop.java
@@ -61,19 +61,31 @@ public class NestedLoop {
             }
         }
         */
-
         System.out.println(" -------   CAR  ------------  ");
 
+        for(int a=0; a<=9; a++) {
 
+            for(int b=0; b<=9; b++) {
 
+                for (int c=0; c<=9; c++) {
 
+                    for (int d=0; d<=9; d++) {
+                        System.out.println(a+" "+b+" "+c+" "+d);
+                    }
+                }
+            }
+        }
 
+        System.out.println(" -------  MULTIPLICATION TABLE  ------------  ");
 
+    for(int i=1; i<=10; i++) {
 
+        for (int j=1; j<=10; j++) {
 
-
-
-
-
+            System.out.println(i+" * "+j+" = "+(i*j));
+        }
+        System.out.println(" ---------------------------- ");
+    }
+    
     }
 }
