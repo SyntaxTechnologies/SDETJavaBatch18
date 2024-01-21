c8dcd3cff0e7836531df22bb8883370ad7d82f31
Asghar <instructor@syntaxtechs.com>
2024-01-21 11:11:27 -0500

    Class 27

---
+++ src/main/java/org/class27/E1Exception.java
@@ -0,0 +1,31 @@
+package org.class27;
+
+public class E1Exception {
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5");
+       // System.out.println(10 / 0);
+        try {
+           // System.out.println(10 / 0);
+            String name=null;
+            System.out.println(name.length());
+        } catch (ArithmeticException e) {
+
+            System.out.println("Please don't try to divide by zero it s not allowed in java");
+        }catch (NullPointerException npe){
+            System.out.println("Make sure your object is initialed properly");
+        }catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("Make sure your index is greater than 0 and less than the size");
+        }catch (Exception e){
+            System.out.println("Something went wrong");
+        }
+
+        System.out.println("6");
+        System.out.println("7");
+        System.out.println("8");
+    }
+}
